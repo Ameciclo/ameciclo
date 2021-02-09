358912e7c7523f9ba1b8382981259f0ff3a4a897
Pedro Paes <iacapuca@gmail.com>
2021-02-09 15:38:57 -0300

    Added alignment styling for Question page

--- frontend/pages/biciclopedia/[question].tsx
+++ frontend/pages/biciclopedia/[question].tsx
@@ -46,10 +46,8 @@ const Questions = ({ question }) => {
             </div>
             <div className="mt-10 py-10 border-t border-gray-300 text-center">
               <div className="flex flex-wrap justify-center">
-                <div className="w-full lg:w-9/12 px-4">
-                  <p className="mb-4 text-lg leading-relaxed text-gray-800">
+                <div className="w-full lg:w-9/12 px-4 mb-4 text-lg leading-relaxed text-gray-800 text-center markdown-align">
                     <ReactMarkdown children={question.answer} />
-                  </p>
                 </div>
               </div>
             </div>
