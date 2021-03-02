9941536f83fc1fa80a2df7e25d1e66842b4a6f61
Daniel Valença <dvalenca@gmail.com>
2021-03-02 14:32:52 -0300

    show date only if has date and format in long pt-br

--- frontend/pages/projetos/[project].tsx
+++ frontend/pages/projetos/[project].tsx
@@ -17,29 +17,36 @@ const PhotoGallery = dynamic(() => import("react-photo-gallery"), {
 });
 
 const ProjectDate = ({ project }) => {
+  
+  const dateOption = {
+    year: 'numeric',
+    month: ('long' ),
+  }
   //console.log("project.partners.length")
 
   //console.log(project.partners.length)
 
   return (
-    <div className="flex flex-row justify-center">
-      <svg className="w-4 h-4 fill-current mr-2" viewBox="0 0 512 512">
-        <defs />
-        <path d="M452 40h-24V0h-40v40H124V0H84v40H60C26.916 40 0 66.916 0 100v352c0 33.084 26.916 60 60 60h392c33.084 0 60-26.916 60-60V100c0-33.084-26.916-60-60-60zm20 412c0 11.028-8.972 20-20 20H60c-11.028 0-20-8.972-20-20V188h432v264zm0-304H40v-48c0-11.028 8.972-20 20-20h24v40h40V80h264v40h40V80h24c11.028 0 20 8.972 20 20v48z" />
-        <path d="M76 230h40v40H76zM156 230h40v40h-40zM236 230h40v40h-40zM316 230h40v40h-40zM396 230h40v40h-40zM76 310h40v40H76zM156 310h40v40h-40zM236 310h40v40h-40zM316 310h40v40h-40zM76 390h40v40H76zM156 390h40v40h-40zM236 390h40v40h-40zM316 390h40v40h-40zM396 310h40v40h-40z" />
-      </svg>
-      <span className="text-lm leading-normal mt-0 mb-2 text-whit e">
-        {new Date(project.startDate).toLocaleDateString()}
-      </span>
-      {project.endDate && (
-        <>
-          <span className="text-white font-bold mx-4">{">"}</span>
-          <span className="text-sm leading-normal mt-0 mb-2 text-white">
-            {new Date(project.endDate).toLocaleDateString()}
-          </span>
-        </>
-      )}
-    </div>
+    project.startDate && (
+      <div className="flex flex-row justify-center">
+        <svg className="w-4 h-4 fill-current mr-2" viewBox="0 0 512 512">
+          <defs />
+          <path d="M452 40h-24V0h-40v40H124V0H84v40H60C26.916 40 0 66.916 0 100v352c0 33.084 26.916 60 60 60h392c33.084 0 60-26.916 60-60V100c0-33.084-26.916-60-60-60zm20 412c0 11.028-8.972 20-20 20H60c-11.028 0-20-8.972-20-20V188h432v264zm0-304H40v-48c0-11.028 8.972-20 20-20h24v40h40V80h264v40h40V80h24c11.028 0 20 8.972 20 20v48z" />
+          <path d="M76 230h40v40H76zM156 230h40v40h-40zM236 230h40v40h-40zM316 230h40v40h-40zM396 230h40v40h-40zM76 310h40v40H76zM156 310h40v40h-40zM236 310h40v40h-40zM316 310h40v40h-40zM76 390h40v40H76zM156 390h40v40h-40zM236 390h40v40h-40zM316 390h40v40h-40zM396 310h40v40h-40z" />
+        </svg>
+        <span className="text-lm leading-normal mt-0 mb-2 text-whit e">
+          {new Date(project.startDate).toLocaleDateString('pt-br', dateOption).toUpperCase()}
+        </span>
+          <>
+            <span className="text-white font-bold mx-4">{"a"}</span>
+            <span className="text-lm leading-normal mt-0 mb-2 text-white">
+            {project.endDate ? (
+              new Date(project.endDate).toLocaleDateString('pt-br', dateOption).toUpperCase()
+              ): "HOJE"}
+            </span>
+          </>
+      </div>
+    )
   );
 };
 
@@ -188,7 +195,9 @@ const Projeto = ({ project }) => {
             <div className="flex flex-wrap justify-center">
               <div className="w-full lg:w-9/12 px-4">
                 <p className="mb-4 text-lg leading-relaxed text-gray-800">
+                  {project.long_description ? (
                   <ReactMarkdown children={project.long_description} />
+                  ): project.description}
                 </p>
               </div>
             </div>
