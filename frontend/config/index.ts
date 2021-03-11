3d1b6ef302f94e1cdc2a50440ff0c1178ee3248b
Daniel Valença <dvalenca@gmail.com>
2021-03-10 21:35:56 -0300

    layout corrections on markdown and partners

--- frontend/config/index.ts
+++ frontend/config/index.ts
@@ -1,4 +1,6 @@
 const dev = process.env.NODE_ENV !== "production";
 export const server = dev
-  ? "http://localhost:1337"
-  : "https://cms.ameciclo.org";
+//  ? "http://localhost:1337"
+//  : "https://cms.ameciclo.org";
+  ? "https://cms.ameciclo.org"
+  : "http://localhost:1337";
