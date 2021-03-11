596cb6a72a7a6bc3d7df594019ef55095959fce1
Pedro Paes <iacapuca@gmail.com>
2021-03-11 17:20:01 -0300

    Create modular stylesheet for Project page

--- frontend/config/index.ts
+++ frontend/config/index.ts
@@ -2,5 +2,3 @@ const dev = process.env.NODE_ENV !== "production";
 export const server = dev
   ? "http://localhost:1337"
   : "https://cms.ameciclo.org";
- // ? "https://cms.ameciclo.org"
- // : "http://localhost:1337";
