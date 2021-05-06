7adc5453f16d7b353f74b9e12447c165381abf9e
Daniel Valença <dvalenca@gmail.com>
2021-05-06 16:10:11 -0300

    filter added

--- frontend/pages/projetos.tsx
+++ frontend/pages/projetos.tsx
@@ -5,21 +5,56 @@ import Breadcrumb from "../components/Breadcrumb";
 import { ProjectCard } from "../components/ProjectCard";
 import { server } from "../config";
 
-const Projetos = ({ projects, footer }) => {
+function string_to_slug (str) {
+  if (str) {
+  str = str.replace(/^\s+|\s+$/g, ''); // trim
+  str = str.toLowerCase();
+
+  // remove accents, swap ñ for n, etc
+  var from = "àáäâãèéëêìíïîòóöôùúüûñç·/_,:;";
+  var to   = "aaaaaeeeeiiiioooouuuunc------";
+  for (var i=0, l=from.length ; i<l ; i++) {
+      str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+  }
+
+  str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+      .replace(/\s+/g, '-') // collapse whitespace and replace by -
+      .replace(/-+/g, '-'); // collapse dashes
+
+  return str;
+  } else {
+    return ""
+  }
+}
+
+const Projetos = ({ projects, workgroups, footer }) => {
   const [filteredProjects, setFilteredProjects] = useState([]);
   const [status, setStatus] = useState("");
+  const [group, setGroup] = useState("");
 
   useEffect(() => {
-    if (status) {
+    if (!group && !status) {
+      setFilteredProjects(projects);
+    } else if(status || group) {
       setFilteredProjects(
         projects.filter((project) => {
-          return project.status === status;
-        })
-      );
-    } else {
-      setFilteredProjects(projects);
+          const selection = {
+            status: status,
+            group: group,
+          }
+          let projectstatus = ""
+          let workgroup = ""
+          if (group && project.workgroup) workgroup = string_to_slug(project.workgroup.name)
+          if (status) projectstatus = project.status
+          if (group === workgroup && status === projectstatus)  {
+            return true
+          } else {
+            return false
+          }      
+          })
+      )
     }
-  }, [status, projects]);
+  }, [status, group, projects]);
 
   return (
     <Layout footer = {footer}>
@@ -46,8 +81,8 @@ const Projetos = ({ projects, footer }) => {
       </div>
       <section className="container my-12 mx-auto">
         <div className="mt-5 mx-3">
-          <div className="inline-block relative w-64">
-            <label htmlFor="status">Selecione o status:</label>
+          <div className="inline-block relative w-64 px-2">
+            <label htmlFor="status">Estado atual</label>
             <select
               value={status}
               name="status"
@@ -61,7 +96,25 @@ const Projetos = ({ projects, footer }) => {
               <option value="paused">Pausado</option>
             </select>
           </div>
+
+          <div className="inline-block relative w-64 px-2">
+            <label htmlFor="group">Grupo de Trabalho:</label>
+            <select
+              value={group}
+              name="group"
+              className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+              onChange={(e) => setGroup(e.target.value)}
+              onBlur={(e) => e}
+            >
+              <option value="">Todos</option>
+              {workgroups.map((wg) => 
+                              <option value={string_to_slug(wg.name)}>{wg.name}</option>
+                              )}
+
+            </select>
+          </div>
         </div>
+
         <div className="mt-5 mx-3 grid grid-cols-1 lg:grid-cols-4 gap-6">
           {filteredProjects
             .sort((a, b) => (a.name > b.name ? 1 : -1))
@@ -88,10 +141,17 @@ export async function getStaticProps() {
     footer = await res_footer.json();
   }
 
+  const res_workgroups = await fetch(`${server}/workgroups`);
+  let workgroups;
+  if (res_workgroups.status === 200) {
+    workgroups = await res_workgroups.json();
+  }
+
   return {
     props: {
       projects,
       footer,
+      workgroups,
     },
     revalidate: 1,
   };
