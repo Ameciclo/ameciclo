0a498bfcb419604083386c23ec7405b5e4f5f8fa
Pedro Paes <plpbs@poli.br>
2022-02-09 12:42:14 -0300

    Fixed issue with API endpoint

--- frontend/config/index.ts
+++ frontend/config/index.ts
@@ -1,2 +1,4 @@
 const dev = process.env.NODE_ENV !== "production";
-export const server = "https://cms.ameciclo.org";
+export const server = dev
+  ? "http://localhost:1337"
+  : "https://cms.ameciclo.org";
