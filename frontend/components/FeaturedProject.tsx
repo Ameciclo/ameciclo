af301b828b1c7f4295e799325a8c7de62f97ea85
Pedro Paes <plpbs@poli.br>
2022-02-17 12:02:18 -0300

    Updated slider component

--- frontend/components/FeaturedProject.tsx
+++ frontend/components/FeaturedProject.tsx
@@ -6,39 +6,41 @@ export const FeaturedProject = ({ project }) => {
   return (
     <>
       <div className="flex min-h-[600px] relative">
-        <Image
-          src={project.media.url}
-          alt={project.name}
-          layout="fill"
-          objectFit="cover"
-        />
-        <div className="container flex justify-center h-full mx-auto my-auto">
-          <div
-            className="relative block rounded-lg shadow-xl bg-gray100Transparent min-h-[18rem] max-w-[850px]"
-            key={project.id}
-          >
-            <div className="flex items-center justify-center mt-5 text-gray-800">
-              <h1 className="text-3xl lg:text-6xl">{project.name}</h1>
-            </div>
-            <div className="mt-5 text-center border-t border-gray-600">
-              <div className="p-6 md:pr-24 md:pl-16 md:py-6">
-                <p
-                  className="text-xl text-gray-800"
-                  style={{
-                    overflow: "hidden",
-                    display: "-webkit-box",
-                    WebkitLineClamp: 3,
-                    WebkitBoxOrient: "vertical",
-                  }}
-                >
-                  {project.description}
-                </p>
-                <Link href={`/projetos/${project.slug}`}>
-                  <a className="flex items-baseline mt-3 text-ameciclo hover:text-red-600 focus:text-red-600">
-                    <span>Conheça mais</span>
-                    <span className="ml-1 text-xs">&#x279c;</span>
-                  </a>
-                </Link>
+        <div className="keen-slider__slide max-h-[600px]">
+          <Image
+            src={project.media.url}
+            alt={project.name}
+            layout="fill"
+            objectFit="cover"
+          />
+          <div className="container flex justify-center h-full mx-auto my-auto">
+            <div
+              className="relative mx-auto my-auto rounded-lg shadow-xl bg-gray100Transparent max-h-[18rem] max-w-[850px]"
+              key={project.id}
+            >
+              <div className="flex items-center justify-center mt-5 text-gray-800">
+                <h1 className="text-3xl lg:text-6xl">{project.name}</h1>
+              </div>
+              <div className="mt-5 text-center border-t border-gray-600">
+                <div className="p-6 md:pr-24 md:pl-16 md:py-6">
+                  <p
+                    className="text-xl text-gray-800"
+                    style={{
+                      overflow: "hidden",
+                      display: "-webkit-box",
+                      WebkitLineClamp: 3,
+                      WebkitBoxOrient: "vertical",
+                    }}
+                  >
+                    {project.description}
+                  </p>
+                  <Link href={`/projetos/${project.slug}`}>
+                    <a className="flex items-baseline mt-3 text-ameciclo hover:text-red-600 focus:text-red-600">
+                      <span>Conheça mais</span>
+                      <span className="ml-1 text-xs">&#x279c;</span>
+                    </a>
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,7 @@
 import Layout from "../components/Layout";
 import SEO from "../components/SEO";
 import Image from "next/image";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { motion } from "framer-motion";
 import { FeaturedProject } from "../components/FeaturedProject";
 import Counter from "../components/Counter";
@@ -10,18 +10,8 @@ import Associe from "../components/Icons/associe";
 import Participe from "../components/Icons/participe";
 import { FeaturedProducts } from "../components/FeaturedProducts";
 import { server } from "../config";
-import { Swiper, SwiperSlide } from "swiper/react";
-import {
-  Navigation,
-  Pagination,
-  Scrollbar,
-  A11y,
-  Autoplay,
-  EffectFade,
-  Lazy,
-} from "swiper";
-import "swiper/swiper-bundle.min.css";
-import "swiper/swiper.min.css";
+import "keen-slider/keen-slider.min.css";
+import { useKeenSlider } from "keen-slider/react";
 
 export default function Home({
   featuredProjects,
@@ -30,6 +20,19 @@ export default function Home({
   home,
   recurrent,
 }) {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [loaded, setLoaded] = useState(false);
+  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
+    initial: 0,
+    loop: true,
+    slideChanged(slider) {
+      setCurrentSlide(slider.track.details.rel);
+    },
+    created() {
+      setLoaded(true);
+    },
+  });
+
   return (
     <Layout>
       <SEO title="Página Principal" />
@@ -76,7 +79,35 @@ export default function Home({
       </section>
       <section>
         <div className="mx-auto">
-          <Swiper
+          <div className="navigation-wrapper">
+            <div ref={sliderRef} className="keen-slider">
+              {featuredProjects.map((p) => (
+                <FeaturedProject project={p} key={p.id} />
+              ))}
+            </div>
+            {loaded && instanceRef.current && (
+              <>
+                <Arrow
+                  left
+                  onClick={(e: any) =>
+                    e.stopPropagation() || instanceRef.current?.prev()
+                  }
+                  disabled={currentSlide === 0}
+                />
+
+                <Arrow
+                  onClick={(e: any) =>
+                    e.stopPropagation() || instanceRef.current?.next()
+                  }
+                  disabled={
+                    currentSlide ===
+                    instanceRef.current.track.details.slides.length - 1
+                  }
+                />
+              </>
+            )}
+          </div>
+          {/* <Swiper
             modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay, Lazy]}
             lazy={true}
             loop={true}
@@ -91,7 +122,7 @@ export default function Home({
                 <FeaturedProject project={p} key={p.id} />
               </SwiperSlide>
             ))}
-          </Swiper>
+          </Swiper> */}
         </div>
       </section>
       <section className="bg-ameciclo">
@@ -106,7 +137,7 @@ export default function Home({
       {featuredProducts.length > 0 && (
         <section>
           <div className="mx-auto">
-            <Swiper
+            {/* <Swiper
               modules={[
                 Navigation,
                 Pagination,
@@ -127,7 +158,7 @@ export default function Home({
                   <FeaturedProducts project={p} key={p.id} />
                 </SwiperSlide>
               ))}
-            </Swiper>
+            </Swiper> */}
           </div>
         </section>
       )}
@@ -188,4 +219,29 @@ export async function getStaticProps() {
       recurrent,
     },
   };
+}
+
+function Arrow(props: {
+  disabled: boolean;
+  left?: boolean;
+  onClick: (e: any) => void;
+}) {
+  const disabeld = props.disabled ? " arrow--disabled" : "";
+  return (
+    <svg
+      onClick={props.onClick}
+      className={`arrow ${
+        props.left ? "arrow--left" : "arrow--right"
+      } ${disabeld}`}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+    >
+      {props.left && (
+        <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
+      )}
+      {!props.left && (
+        <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
+      )}
+    </svg>
+  );
 }
