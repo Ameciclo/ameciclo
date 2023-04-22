4f6ba6d2fc8a8bf31520ec88b255557a9ebabcc0
Pedro Paes <plpbs@poli.br>
2023-04-22 18:51:56 -0300

    Updated config folder

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '25a4e47da7c3bcd960e61cd75c093f64'),
-    },
+  app: {
+    keys: env.array('APP_KEYS')
   },
-});
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false)
+  }
+})
