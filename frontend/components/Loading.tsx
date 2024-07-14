406b62dc60ebb91ddfbf59eeb0afd57745926a0d
Italo Sergio <italosergio@mail.com>
2024-07-14 09:14:47 -0300

    add: Loading component

---
+++ frontend/components/Loading.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Header from "./Header";
+import { Footer } from "./Footer";
+
+const Loading = () => {
+  return (
+    <>
+      <div className="flex flex-col justify-between h-screen font-sans text-gray-900">
+        <Header />
+        <div className=' flex flex-col w-full h-full items-center justify-end bg-ameciclo'>
+          <img
+            src="/android-icon-192x192.png"
+            alt="icone ameciclo girando em torno do proprio eixo infinitamente"
+            className='animate-bounce rounded-full xl:w-96'
+          />
+        </div>
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default Loading;
