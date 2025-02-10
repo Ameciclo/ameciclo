879cee161f4577864e3dce0f1cfb905f2e70e28a
Italo Sergio <italosergio@mail.com>
2025-02-10 14:58:20 -0300

    concertando pagina quebrada quando da erro na captura dos dados pela api

--- frontend/app/components/Home/SectionCallToAction.tsx
+++ frontend/app/components/Home/SectionCallToAction.tsx
@@ -19,22 +19,36 @@ export async function loader() {
 
     const home: HomeData = await response.json();
 
-    return ({ home });
+    return json({ home });
 }
 
 export default function SectionCallToAction() {
+    try {
+        const { home } = useLoaderData<typeof loader>();
 
-    const { home } = useLoaderData<typeof loader>();
-
-    return (
-        <section className="bg-ameciclo">
-            <div className="container px-6 py-20 mx-auto">
-                <div className="flex flex-wrap justify-around">
-                    <Link className="buttom-call-actions" to={home.participation_url}><Participe /></Link>
-                    <Link className="buttom-call-actions" to={home.association_url}><Associe /></Link>
-                    <Link className="buttom-call-actions" to={home.donate_url}><Apoie /></Link>
+        return (
+            <section className="bg-ameciclo">
+                <div className="container px-6 py-20 mx-auto">
+                    <div className="flex flex-wrap justify-around">
+                        <Link className="buttom-call-actions" to={home?.participation_url}><Participe /></Link>
+                        <Link className="buttom-call-actions" to={home?.association_url}><Associe /></Link>
+                        <Link className="buttom-call-actions" to={home?.donate_url}><Apoie /></Link>
+                    </div>
                 </div>
-            </div>
-        </section>
-    );
+            </section>
+        );
+    } catch (err) {
+        return (
+            <section className="bg-ameciclo">
+                <a href="/status">!</a>
+                <div className="container px-6 py-20 mx-auto">
+                    <div className="flex flex-wrap justify-around">
+                        <Link className="buttom-call-actions" to="https://participe.ameciclo.org"><Participe /></Link>
+                        <Link className="buttom-call-actions" to="https://queroser.ameciclo.org"><Associe /></Link>
+                        <Link className="buttom-call-actions" to="https://apoia.se/ameciclo"><Apoie /></Link>
+                    </div>
+                </div>
+            </section>
+        );
+    }
 }
