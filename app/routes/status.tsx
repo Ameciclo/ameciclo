6c305224df7c489eb2e1d4c7ac76bff02fe7dba3
Italo Sergio <italosergio@mail.com>
2025-02-18 18:35:09 -0300

    Incrementa novos pontos de monitoramento

--- app/routes/status.tsx
+++ app/routes/status.tsx
@@ -1,12 +1,7 @@
-import type { LoaderFunctionArgs, MetaFunction } from '@remix-run/node';
-import { json } from '@remix-run/node';
+import type { MetaFunction } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
-
-interface ServiceStatus {
-    name: string;
-    url: string;
-    status: 'OK' | 'OFF';
-}
+import { loader } from "../loader/status";
+export { loader };
 
 // Meta tags para a página
 export const meta: MetaFunction = () => {
@@ -16,55 +11,21 @@ export const meta: MetaFunction = () => {
     ];
 };
 
-// Loader para verificar o status dos serviços
-export async function loader({ request }: LoaderFunctionArgs) {
-    // Lista de serviços para verificar
-    const services = [
-        { name: 'Página - Página Inicial', url: 'ameciclo.org' },
-        { name: 'Serviço Externo - Associe-se', url: 'docs.google.com/forms/d/e/1FAIpQLSeBboZ6fDhGEuJjVSyt7r3tTe5FF8VJH1gKt95jq6JslrwOdQ/viewform' },
-        { name: 'Serviço Externo - Participe', url: 'participe.ameciclo.org' },
-    ];
-
-    // Função para verificar o status de um serviço
-    const checkStatus = async (url: string): Promise<'OK' | 'OFF'> => {
-        try {
-            const response = await fetch(`https://${url}`, { method: 'HEAD' });
-            return response.ok ? 'OK' : 'OFF';
-        } catch (error) {
-            return 'OFF';
-        }
-    };
-
-    // Verificar o status de todos os serviços
-    const servicesWithStatus = await Promise.all(
-        services.map(async (service) => {
-            const status = await checkStatus(service.url);
-            return { ...service, status };
-        }),
-    );
-
-    // Retornar os dados para o componente
-    return json({ services: servicesWithStatus });
-}
-
 // Componente da página
 export default function StatusPage() {
     const { services } = useLoaderData<typeof loader>();
 
     return (
         <div className="" style={{ fontFamily: 'Arial, sans-serif', padding: '20px' }}>
-            <br />
-            <br />
-            <br />
             <h1 style={{ fontSize: '24px', marginBottom: '20px' }}>Status dos Serviços</h1>
             {services.map((service, index) => (
                 <div key={index} style={{ marginBottom: '10px' }}>
                     <p>
-                        <strong>{service.name}:</strong>{' '}
-                        <strong>{service.url}</strong>{'    '}
                         <span style={{ color: service.status === 'OK' ? 'green' : 'red' }}>
-                            {service.status}
-                        </span>
+                            <strong>[{service.status}]</strong>
+                        </span>{' '}
+                        <strong>{service.name}:</strong>{' '}
+                        <strong style={{ color: service.status === 'OK' ? 'green' : 'red' }}>{service.url}</strong>
                     </p>
                 </div>
             ))}
