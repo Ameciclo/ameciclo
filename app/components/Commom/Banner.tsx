4f165b40a9e8c65ef6231a9784bcd6f9f8a38f45
Italo Sergio <italosergio@mail.com>
2025-03-17 14:43:44 -0300

    refatoração do componente Banner para torna-lo mais genérico

--- app/components/Commom/Banner.tsx
+++ app/components/Commom/Banner.tsx
@@ -1,9 +1,9 @@
-export default function Banner() {
+export default function Banner({image, alt = "image banner"}: any) {
     return (
         <section className="h-[70vh] w-full relative overflow-hidden py-[58px]">
             <img
-                src="/backgroundImage.webp"
-                alt="Ameciclo Banner"
+                src={image}
+                alt={alt}
                 className="absolute inset-0 w-full h-full object-cover"
             />
             <div className="w-96 h-96 bg-black"></div>
