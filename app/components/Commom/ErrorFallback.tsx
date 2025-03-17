8efd913f58f5e8b913a55aa9a5b4975194fb88d7
Italo Sergio <italosergio@mail.com>
2025-03-17 20:41:33 -0300

    cria página componente error

---
+++ app/components/Commom/ErrorFallback.tsx
@@ -0,0 +1,19 @@
+import { useRouteError, isRouteErrorResponse } from "@remix-run/react";
+
+export default function ErrorFallback() {
+    const error = useRouteError();
+
+    return (
+        <div className="flex flex-col pt-16 items-center justify-start h-max text-gray-900">
+            <h1 className="text-5xl font-bold pb-16">Ocorreu um erro 😢</h1>
+            {isRouteErrorResponse(error) ? (
+                <>
+                    <p className="text-3xl font-bold pb-6">{error.status}</p>
+                    {error.data?.message && <p>Detalhes: {error.data.message}</p>}
+                </>
+            ) : (
+                <p>Algo deu errado. Tente novamente mais tarde.</p>
+            )}
+        </div>
+    );
+}
