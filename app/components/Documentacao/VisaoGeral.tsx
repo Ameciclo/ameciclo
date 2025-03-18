d637b19322107fa52dc5c96115e0df100d10608a
Italo Sergio <italosergio@mail.com>
2025-03-17 21:52:10 -0300

    cria componente de visão geral para página de documentação

---
+++ app/components/Documentacao/VisaoGeral.tsx
@@ -0,0 +1,13 @@
+// src/components/VisaoGeral.tsx
+export default function VisaoGeral() {
+  return (
+    <section className="p-6">
+      <h2 className="text-3xl font-bold text-gray-800 mb-4">Visão Geral</h2>
+      <p className="text-gray-700 text-lg mb-4">
+        Este projeto é um aplicativo web desenvolvido utilizando <strong>Remix</strong> e <strong>TypeScript</strong>.
+        Ele tem como objetivo fornecer dados sobre mobilidade ativa na região metropolitana do Recife. O sistema é
+        estruturado de forma modular, garantindo escalabilidade e manutenção eficiente.
+      </p>
+    </section>
+  );
+}
