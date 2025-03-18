ec55928f18a647febc1f22797be120992e3c8464
Italo Sergio <italosergio@mail.com>
2025-03-18 15:12:01 -0300

    criação de componente Título para página de Documentação

--- app/components/Documentacao/Titulo.tsx
+++ app/components/Documentacao/Titulo.tsx
@@ -1,5 +1,8 @@
-export default function Titulo({title}: any) {
+export default function Titulo({ title }: any) {
     return (
-        <h1 className="text-4xl text-center font-bold mb-4">{title}</h1>
+        <>
+            <h1 className="text-4xl pt-5 text-center font-bold mb-4">{title}</h1>
+            <h4 className="pb-5 text-center">Em desenvolvimento....</h4>
+        </>
     )
 }
