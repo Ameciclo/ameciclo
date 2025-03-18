792994264dad6f19f9b9f0c38e5801e74dabae26
Italo Sergio <italosergio@mail.com>
2025-03-17 22:26:50 -0300

    corrige erro de build no componente root

--- app/root.tsx
+++ app/root.tsx
@@ -9,7 +9,7 @@ import type { LinksFunction, MetaFunction } from "@remix-run/node";
 import { Navbar } from "./components/Commom/Navbar";
 import { Footer } from "./components/Commom/Footer";
 import { GoogleAnalytics } from "./components/Commom/GoogleAnalytics";
-import styles from "./tailwind.css";
+import "./tailwind.css";
 
 export const meta: MetaFunction = () => {
   return [
@@ -34,20 +34,6 @@ export const meta: MetaFunction = () => {
     },
   ];
 };
-
-export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: styles },
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-];
 
 export default function App() {
   return (
