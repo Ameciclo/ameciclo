1a3ef97ada7139e311a77ba5504e7fcbe305fa2f
Italo Sergio <italosergio@mail.com>
2025-03-18 15:10:32 -0300

    melhoramento de página de documentação

--- app/routes/documentacao.tsx
+++ app/routes/documentacao.tsx
@@ -3,12 +3,14 @@ import Indice from "app/components/Documentacao/Indice";
 import VisaoGeral from "~/components/Documentacao/VisaoGeral";
 import EstruturaDoProjeto from "~/components/Documentacao/EstruturaDoProjeto";
 import Conclusao from "~/components/Documentacao/Conclusao";
+import Banner from "~/components/Commom/Banner";
 
 export default function Docs() {
   return (
     <>
       <div className="p-6 mt-5 mb-5 max-w-5xl mx-auto bg-gray-100 rounded shadow-lg">
         <Titulo title="Documentação do Projeto para Desenvolvedores" />
+        <Banner />
         <VisaoGeral />
         <Indice />
         <EstruturaDoProjeto />
