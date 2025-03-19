66aadac0386fb4c3c5f4e3bca60e56cfd6513de6
Italo Sergio <italosergio@mail.com>
2025-03-19 18:02:33 -0300

    Adiciona componente Banner à página contagens em dados /dados/contagens

--- app/routes/dados.contagens.tsx
+++ app/routes/dados.contagens.tsx
@@ -1,3 +1,23 @@
+import { json, LoaderFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import Banner from "~/components/Commom/Banner";
+
+export const loader: LoaderFunction = async () => {
+    const res = await fetch("https://cms.ameciclo.org/contagens", {
+        cache: "no-cache",
+    });
+
+    if (!res.ok) {
+        throw new Response("Erro ao buscar os dados", { status: res.status });
+    }
+
+    const data = await res.json();
+    return json({ cover: data.cover });
+};
+
+
 export default function Contagens() {
-    return (<p>Página Documentos</p>);
+    const { cover } = useLoaderData<typeof loader>();
+
+    return (<Banner image={cover?.url} alt="Capa da plataforma de dados" />);
 }
