c05f9db4c129e7aeecffdb47eb4c6af01a297ae3
Italo Sergio <italosergio@mail.com>
2025-03-19 18:27:21 -0300

    Adiciona componente Banner à página ideciclo em dados /dados/ideciclo

--- app/routes/dados.ideciclo.tsx
+++ app/routes/dados.ideciclo.tsx
@@ -1,3 +1,23 @@
+import { json, LoaderFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import Banner from "~/components/Commom/Banner";
+
+export const loader: LoaderFunction = async () => {
+    const res = await fetch("https://cms.ameciclo.org/ideciclo", {
+        cache: "no-cache",
+    });
+
+    if (!res.ok) {
+        throw new Response("Erro ao buscar os dados", { status: res.status });
+    }
+
+    const data = await res.json();
+    return json({ cover: data.cover });
+};
+
+
 export default function Ideciclo() {
-    return (<p>Página Ideciclo</p>);
+    const { cover } = useLoaderData<typeof loader>();
+
+    return (<Banner image={cover?.url} alt="Capa da página do Ideciclo" />);
 }
