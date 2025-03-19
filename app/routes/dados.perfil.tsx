efc3de18290cc40d15b85c39de9a967d1ff548a1
Italo Sergio <italosergio@mail.com>
2025-03-18 22:43:37 -0300

    Adiciona Banner da páagina de dados de perfil do ciclista

--- app/routes/dados.perfil.tsx
+++ app/routes/dados.perfil.tsx
@@ -1,3 +1,23 @@
-export default function Perfil() {
-    return (<p>Página Perfil</p>);
+import { json, LoaderFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import Banner from "~/components/Commom/Banner";
+
+export const loader: LoaderFunction = async () => {
+    const res = await fetch("https://cms.ameciclo.org/perfil", {
+        cache: "no-cache",
+    });
+
+    if (!res.ok) {
+        throw new Response("Erro ao buscar os dados", { status: res.status });
+    }
+
+    const data = await res.json();
+    return json({ cover: data.cover });
+};
+
+
+export default function perfil() {
+    const { cover } = useLoaderData<typeof loader>();
+
+    return (<Banner image={cover?.url} alt="Capa da página de Perfil do Ciclista" />);
 }
