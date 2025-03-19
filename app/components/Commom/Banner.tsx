98e7ba35e2c493148ce1228f2969425f8bbb1022
Italo Sergio <italosergio@mail.com>
2025-03-18 22:41:05 -0300

    Melhora Banner

--- app/components/Commom/Banner.tsx
+++ app/components/Commom/Banner.tsx
@@ -1,13 +1,12 @@
-export default function Banner({image="backgroundImage.webp", alt = "image banner"}: any) {
-    return (
-        <div className="relative py-24 w-full h-[52vh]">
-        <img
-          src={image}
-          alt={alt}
-          className="absolute inset-0 object-cover w-full h-full"
-          loading="lazy"
-        />
-      </div>
-    )
+export default function Banner({ image = "backgroundImage.webp", alt = "image banner" }: any) {
+  return (
+    <div className="relative py-24 w-full h-[52vh]">
+      <img
+        src={image || "projetos.webp"} // Fallback caso image seja undefined
+        alt={alt}
+        className="absolute inset-0 object-cover w-full h-full"
+        loading="lazy"
+      />
+    </div>
+  );
 }
-
