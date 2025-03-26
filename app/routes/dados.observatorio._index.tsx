e8f199897c3d519e73b5ab802aef9e8fee417d83
Italo Sergio <italosergio@mail.com>
2025-03-26 16:04:06 -0300

    cria rota /observatorio

---
+++ app/routes/dados.observatorio._index.tsx
@@ -0,0 +1,29 @@
+import { json, LoaderFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import Banner from "~/components/Commom/Banner";
+import Breadcrumb from "~/components/Commom/Breadcrumb";
+
+export const loader: LoaderFunction = async () => {
+    const res = await fetch("https://cms.ameciclo.org/plataforma-de-dados", {
+        cache: "no-cache",
+    });
+
+    if (!res.ok) {
+        throw new Response("Erro ao buscar os dados", { status: res.status });
+    }
+
+    const data = await res.json();
+    return json({ cover: data.cover });
+};
+
+
+export default function Observatorio() {
+    const { cover } = useLoaderData<typeof loader>();
+
+    return (
+        <>
+            <Banner image={cover?.url} alt="Capa da página do Observatório" />
+            <Breadcrumb label="Observatório" slug="/dados/observatorio" routes={["/", "/dados"]} />
+        </>
+    );
+}
