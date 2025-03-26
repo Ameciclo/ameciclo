34733b4984f4bddeffc7af4ac6daad265b668183
Italo Sergio <italosergio@mail.com>
2025-03-26 16:05:44 -0300

    cria página /dados/observatorio/dom

--- app/routes/dados.observatorio.dom.tsx
+++ app/routes/dados.observatorio.dom.tsx
@@ -1,3 +1,29 @@
+import { json, LoaderFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import Banner from "~/components/Commom/Banner";
+import Breadcrumb from "~/components/Commom/Breadcrumb";
+
+export const loader: LoaderFunction = async () => {
+    const res = await fetch("https://cms.ameciclo.org/plataforma-de-dados", {
+        cache: "no-cache",
+    });
+
+    if (!res.ok) {
+        throw new Response("Erro ao buscar os dados", { status: res.status });
+    }
+
+    const data = await res.json();
+    return json({ cover: data.cover });
+};
+
+
 export default function Dom() {
-    return (<p>Página Diagnóstico Orcamentário Municipal</p>);
+    const { cover } = useLoaderData<typeof loader>();
+
+    return (
+        <>
+            <Banner image={cover?.url} alt="Capa da página do Diágnóstico Orçamentário Municipal" />
+            <Breadcrumb label="dom" slug="/dados/observatorio/dom" routes={["/", "/dados", "/dados/observatorio"]} />
+        </>
+    );
 }
