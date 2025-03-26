c7bbe789ceee76324a788083de0c31b764501290
Italo Sergio <italosergio@mail.com>
2025-03-26 16:06:07 -0300

    cria páginas /dados/observatorio/loaclima

--- app/routes/dados.observatorio.loaclima.tsx
+++ app/routes/dados.observatorio.loaclima.tsx
@@ -1,3 +1,29 @@
-export default function LoaClima() {
-    return (<p>Página LoaClima</p>);
+import { json, LoaderFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import Banner from "~/components/Commom/Banner";
+import Breadcrumb from "~/components/Commom/Breadcrumb";
+
+export const loader: LoaderFunction = async () => {
+    const res = await fetch("https://cms.ameciclo.org/plataforma-de-dados", {
+        cache: "no-cache",
+    });
+
+    if (!res.ok) {
+        throw new Response("Erro ao buscar os dados", { status: res.status });
+    }
+
+    const data = await res.json();
+    return json({ cover: data.cover });
+};
+
+
+export default function Loaclima() {
+    const { cover } = useLoaderData<typeof loader>();
+
+    return (
+        <>
+            <Banner image={cover?.url} alt="Capa da página do Loaclima" />
+            <Breadcrumb label="loaclima" slug="/dados/observatorio/loaclima" routes={["/", "/dados", "/dados/observatorio"]} />
+        </>
+    );
 }
