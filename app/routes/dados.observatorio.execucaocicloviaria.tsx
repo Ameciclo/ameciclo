79d38825c89004c3c463f424a54f66bd5133efaf
Italo Sergio <italosergio@mail.com>
2025-03-27 18:37:30 -0300

    implementa componente ExplanationBoxes na página /dados/observatorio/execucaocicloviaria

--- app/routes/dados.observatorio.execucaocicloviaria.tsx
+++ app/routes/dados.observatorio.execucaocicloviaria.tsx
@@ -2,9 +2,10 @@ import { json, LoaderFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import Banner from "~/components/Commom/Banner";
 import Breadcrumb from "~/components/Commom/Breadcrumb";
+import { ExplanationBoxes } from "~/components/Dados/ExplanationBoxes";
 
 export const loader: LoaderFunction = async () => {
-    const res = await fetch("https://cms.ameciclo.org/projects", {
+    const res = await fetch("https://cms.ameciclo.org/plataforma-de-dados", {
         cache: "no-cache",
     });
 
@@ -13,16 +14,23 @@ export const loader: LoaderFunction = async () => {
     }
 
     const data = await res.json();
-    return json({ cover: data.cover });
+    const { cover, description } = data;
+
+    return json({
+        cover,
+        description
+    });
 };
 
 export default function ExecucaoCicloviaria() {
-    const { cover } = useLoaderData<typeof loader>();
+    const { cover, description } = useLoaderData<typeof loader>();
 
     return (
         <>
             <Banner image={cover?.url} alt="Capa da página dos dados, de execuções cicloviárias, na região metropolitana do recife." />
             <Breadcrumb label="Execução Cicloviária" slug="/execucaocicloviaria" routes={["/", "/dados", "/dados/observatório"]} />
-        </>
+            <ExplanationBoxes boxes={[{ title: "O que temos aqui?", description }]} />
+        </>            
+
     );
 }
