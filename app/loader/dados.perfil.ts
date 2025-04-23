aed5eb4585b8544f6cde28c45e884d922cb8ed44
Italo Sergio <italosergio@mail.com>
2025-04-23 18:11:46 -0300

    cria loader de página de perfil /dados/perfil

---
+++ app/loader/dados.perfil.ts
@@ -0,0 +1,16 @@
+import { json, LoaderFunction } from "@remix-run/node";
+
+export const loader: LoaderFunction = async () => {
+    try {
+        const res = await fetch("https://cms.ameciclo.org/perfil", {
+            cache: "no-cache",
+        });
+
+        const data = await res.json();
+        const { cover, description } = data;
+        return json({ cover, description });
+    } catch (error) {
+        console.error("Erro no loader:", error);
+        throw json({ message: "Strapi error - Erro ao buscar os dados" }, { status: 500 });
+    }
+};
