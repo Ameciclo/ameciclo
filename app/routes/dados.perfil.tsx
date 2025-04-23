6d3ddedf8071859701922633ab23b3646ff5e047
Italo Sergio <italosergio@mail.com>
2025-04-23 18:12:20 -0300

    modifica usabilidade de loader de página de perfil /dados/perfil

--- app/routes/dados.perfil.tsx
+++ app/routes/dados.perfil.tsx
@@ -1,21 +1,9 @@
-import { json, LoaderFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import Banner from "~/components/Commom/Banner";
 import Breadcrumb from "~/components/Commom/Breadcrumb";
 
-export const loader: LoaderFunction = async () => {
-    const res = await fetch("https://cms.ameciclo.org/perfil", {
-        cache: "no-cache",
-    });
-
-    if (!res.ok) {
-        throw new Response("Erro ao buscar os dados", { status: res.status });
-    }
-
-    const data = await res.json();
-    return json({ cover: data.cover });
-};
-
+import { loader } from "~/loader/dados.perfil";
+export { loader };
 
 export default function perfil() {
     const { cover } = useLoaderData<typeof loader>();
