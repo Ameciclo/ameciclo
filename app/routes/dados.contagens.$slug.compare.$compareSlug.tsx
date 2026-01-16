8cc66f1f01c73e3178188be83bb1e8b857474579
Ítalo Sérgio <italosergio@mail.com>
2026-01-16 07:59:32 -0300

    fix(contagens): corrige breadcrumb na página de comparação de contagens
    Corrige links quebrados no breadcrumb que redirecionavam para URLs com 'undefined'.
    
    - Breadcrumb gerava links como /dados/contagens/undefined
    - Código tentava acessar count.slug mas API retorna location.id
    
    - Atualiza breadcrumb para usar location?.id ao invés de count.slug
    - Adiciona optional chaining e fallbacks para evitar undefined
    - Remove interfaces não utilizadas (CountEdition, CountEditionSummary, etc.)
    
    - Corrige mapeamento de dados no breadcrumb
    - Remove 58 linhas de interfaces desnecessárias
    - Adiciona tratamento seguro com optional chaining

--- app/routes/dados.contagens.$slug.compare.$compareSlug.tsx
+++ app/routes/dados.contagens.$slug.compare.$compareSlug.tsx
@@ -18,64 +18,6 @@ interface Series {
   visible?: boolean;
 }
 
-interface CountEditionSummary {
-  max_hour: number;
-  total_cyclists: number;
-  total_cargo: number;
-  total_helmet: number;
-  total_juveniles: number;
-  total_motor: number;
-  total_ride: number;
-  total_service: number;
-  total_shared_bike: number;
-  total_sidewalk: number;
-  total_women: number;
-  total_wrong_way: number;
-}
-
-interface CountEditionCoordinates {
-  point: {
-    x: number;
-    y: number;
-  };
-  type: string;
-  name: string;
-}
-
-interface CountEditionSession {
-  start_time: string;
-  end_time: string;
-  total_cyclists: number;
-  characteristics: {
-    [key: string]: number;
-  };
-  quantitative: {
-    [key: string]: number;
-  };
-}
-
-interface CountEditionDirections {
-  origin: string;
-  destin: string;
-  origin_cardinal: string;
-  destin_cardinal: string;
-}
-
-interface CountEdition {
-  id: number;
-  slug: string;
-  name: string;
-  date: string;
-  summary: CountEditionSummary;
-  coordinates: CountEditionCoordinates[];
-  sessions: {
-    [key: string]: CountEditionSession;
-  };
-  directions: {
-    [key: string]: CountEditionDirections;
-  };
-}
-
 type pointData = {
   key: string;
   latitude: number;
@@ -216,9 +158,9 @@ export default function Compare() {
                         <path d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z" />
                       </svg>
                     </li>
-                    {data.map((count: CountEdition, index: number) => (
+                    {data.map((location: any, index: number) => (
                       <li key={index} className="flex items-center">
-                        <Link to={`/dados/contagens/${count.slug}`} className="text-white">{count.name}</Link>
+                        <Link to={`/dados/contagens/${location?.id || ''}`} className="text-white">{location?.name || 'Contagem'}</Link>
                         <svg className="fill-current w-3 h-3 mx-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
                           <path d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z" />
                         </svg>
